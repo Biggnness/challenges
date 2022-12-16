30c41c01e75df14971554e45d071f50eb52aaed2
Brandon Joseph <bradnonjdevs@gmail.com>
2022-12-15 23:21:37 -0500

    Lost Map Challenge

---
+++ lostNoMap.js
@@ -0,0 +1,9 @@
+//Given an array of integers, return a new array with each value doubled.
+
+//For example:
+
+//[1, 2, 3] --> [2, 4, 6]
+
+function maps(x){ //create a funtion that takes an array
+    return x.map(x => x*2) //map funcntion to return an array thats doubles
+  }
