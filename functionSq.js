43f763f9ca15f10d99155b944e3783ebb46695be
Brandon Joseph <bradnonjdevs@gmail.com>
2022-12-20 23:53:49 -0500

    Square the Number

---
+++ functionSq.js
@@ -0,0 +1,11 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+//My Solution
+// Write the "square"-function here
+function square(n){ //create a fucntion that takes in the n argument
+  let result = n**2 //declare the variable and provide the math
+  return result //resturn result
+}
+
+//Other Solutions
+//const square = (n) => n * n;f
